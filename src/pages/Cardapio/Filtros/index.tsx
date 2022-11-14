f373257195cf3ada030f96adea533ac9ecd3b26f
olliso17 <olli.so17@gmail.com>
2022-11-14 09:14:23 -0300

    alteração no filtro

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,25 +1,35 @@
 import styles from './Filtros.module.scss';
 import filtros from './filtros.json';
+import classNames from 'classnames';
 
 //duas formas de tipar opcao
 
 type IOpcao = typeof filtros[0];
 
-// interface Opcao{
+// interface IOpcao{
 //     id:number,
 //     label:string,
 // }
 
-export default function Filtros(){
+interface Props{
+    filtro:number|null,
+    setFiltro:React.Dispatch<React.SetStateAction<number | null>>,
+}
+
+export default function Filtros({filtro, setFiltro}:Props){
 
     function selecionarFiltro(opcao:IOpcao){ 
-
+        if(filtro ===opcao.id)return setFiltro(null);
+        return setFiltro(opcao.id);
     }
 
     return (
         <div className={styles.filtros}>
             {filtros.map((opcao)=>(
-                <button className={styles.filtos__filtro} key={opcao.id} onClick={()=>selecionarFiltro(opcao)}>   
+                <button className={classNames({
+                    [styles.filtros__filtro]:true,
+                    [styles["filtros__filtro--ativo"]]:filtro===opcao.id
+                })} key={opcao.id} onClick={()=>selecionarFiltro(opcao)}>   
                     {opcao.label}
                 </button>
             ))}
