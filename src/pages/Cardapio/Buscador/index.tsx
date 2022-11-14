b8ddd553899684c712ade8134917e19ac4a5b5f7
olliso17 <olli.so17@gmail.com>
2022-11-14 09:34:21 -0300

    criando um ordenador

--- src/pages/Cardapio/Buscador/index.tsx
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -10,7 +10,7 @@ interface Props{
 export default function Buscador({busca, setBusca}:Props){
     return(
         <div className={styles.buscador}>
-            <input value={busca} onChange={evento=>setBusca(evento.target.value)} />
+            <input value={busca} onChange={evento=>setBusca(evento.target.value)} placeholder="Buscar"/>
                 <CgSearch size={20} color="#4C4D5E"/>
         </div>
     );
